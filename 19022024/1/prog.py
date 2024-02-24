5790b975846c44325a1e671a0e0838086e3c930c
Step <osotoff.stepan@yandex.ru>
2024-02-24 23:10:08 +0300

    Add 1 part of task

--- 19022024/1/prog.py
+++ 19022024/1/prog.py
@@ -5,8 +5,16 @@ import glob
 
 
 if len(sys.argv) == 1:
-	print("There are too few arguments.")
-elif len(sys.argv) == 2:
-	pass
+    print("There are too few arguments.")
+    exit(0)
+
+path = sys.argv[1] + '/.git/refs/heads'
+
+if len(sys.argv) == 2:
+    for item in glob.iglob(path):
+        direc = os.scandir(item)
+
+        for branch in direc:
+            print(branch.name)
 else:
-	pass
+    pass
