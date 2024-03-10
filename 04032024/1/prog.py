2d642e02428f3b11c3d12616795089145c161f9c
step <step@Ascorbinka.ascorbinka.org>
2024-03-10 16:41:12 +0300

    Add parser

--- 04032024/1/prog.py
+++ 04032024/1/prog.py
@@ -1,4 +1,5 @@
 import cowsay
+import shlex
 
 
 field = [[0 for j in range(10)] for i in range(10)]
@@ -6,14 +7,13 @@ allowed_list = cowsay.list_cows()
 
 
 def encounter(x, y):
-    out = field[y][x].split()
-    print(cowsay.cowsay(out[1], cow=out[0]))
+    print(cowsay.cowsay(field[y][x]['hello'], field[y][x]['name']))
 
 
 x, y = 0, 0
 
 while inp := input():
-    inp = inp.split()
+    inp = shlex.split(inp)
 
     moved = 0
     if inp[0] == 'up':
@@ -36,30 +36,49 @@ while inp := input():
             encounter(x, y)
     else:
         if inp[0] == 'addmon':
-            if len(inp) < 5:
+            if len(inp) != 9:
                 print("Invalid arguments")
                 continue
 
-            try:
-                m_x = int(inp[2])
-                m_y = int(inp[3])
+            name = inp[1]
+            hello = ''
+            hp = 0
+            m_x, m_y = 0, 0
 
-                if m_x < 0 or m_x > 9 or m_y < 0 or m_y > 9:
-                    raise Exception
-            except Exception:
-                print("Invalid arguments")
+            i = 2
+            while i < 9:
+                if inp[i] == 'hello':
+                    hello = inp[i+1]
+
+                    i += 2
+                elif inp[i] == 'hp':
+                    try:
+                        hp = int(inp[i+1])
+                    except Exception:
+                        break
+
+                    if hp <= 0:
+                        break
+
+                    i += 2
+                elif inp[i] == 'coords':
+                    try:
+                        m_x = int(inp[i+1])
+                        m_y = int(inp[i+2])
+                    except Exception:
+                        break
+
+                    if m_x < 0 or m_x > 9 or m_y < 0 or m_y > 9:
+                        break
+
+                    i += 3
+                else:
+                    print("Invalid arguments")
+                    break
+
+            if i < 9:
                 continue
 
-            if inp[3] not in allowed_list:
-                print("Invalid arguments")
-                continue
-
-            if field[m_y][m_x] == 0:
-                print(f'Added monster to ({m_x}, {m_y}) saying {inp[4]}')
-            else:
-                print(f'Replaced the old monster')
-
-            field[m_y][m_x] = inp[1] + ' ' + inp[4]
-
+            field[m_y][m_x] = {'hello':hello, 'hp': hp, 'name': name}
         else:
             print('Invalid command')
