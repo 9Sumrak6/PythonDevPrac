5b1178928e703d706f8efef482befe2bb62d1ed6
step <step@Ascorbinka.ascorbinka.org>
2024-03-16 20:45:16 +0300

    Change structure of class

--- 11032024/1/prog.py
+++ 11032024/1/prog.py
@@ -32,6 +32,64 @@ class Mud(cmd.Cmd):
         self.allowed_list = cowsay.list_cows()
         self.user_list = {'jgsbat': self.jgsbat}
 
+    def get_mon_args(self, args):
+        args = shlex.split(args)
+
+        default = ('', '', 0, -1, -1)
+        name, hello, hp, m_x, m_y  = default
+
+        if len(args) != 8:
+            print("Invalid arguments")
+            return default
+
+        name = args[0]
+
+        if name not in self.allowed_list and name not in self.user_list:
+            print("Invalid arguments")
+            return default
+
+        i = 1
+        while i < 8:
+            if args[i] == 'hello':
+                hello = args[i+1]
+            elif args[i] == 'hp':
+                try:
+                    hp = int(args[i+1])
+                except Exception:
+                    return default
+
+                if hp <= 0:
+                    return default
+            elif args[i] == 'coords':
+                try:
+                    m_x = int(args[i+1])
+                    m_y = int(args[i+2])
+                except Exception:
+                    return default
+
+                if m_x < 0 or m_x > 9 or m_y < 0 or m_y > 9:
+                    return default
+
+                i += 1
+            else:
+                print("Invalid arguments")
+                return default
+            i += 2
+
+        if i < 8:
+            return default
+
+        return (name, hello, hp, m_x, m_y)
+
+
+    def move_mon(self, name, hello, hp, m_x, m_y):
+        if self.field[m_y][m_x] == 0:
+            print(f'Added monster to ({m_x}, {m_y}) saying {hello}')
+        else:
+            print(f'Replaced the old monster')
+
+        self.field[m_y][m_x] = {'hello':hello, 'hp': hp, 'name': name}
+
 
     def encounter(self, x, y):
         print(f'Moved to ({x}, {y})')
@@ -49,80 +107,44 @@ class Mud(cmd.Cmd):
             print(cowsay.cowsay(hello, cowfile=self.user_list[name]))
 
 
-    def do_up(self, args):
+    def move_up(self):
         self.y = (self.y - 1) % 10
         self.encounter(self.x, self.y)
 
 
-    def do_down(self, args):
+    def move_down(self):
         self.y = (self.y + 1) % 10
         self.encounter(self.x, self.y)
 
 
-    def do_right(self, args):
+    def move_right(self):
         self.x = (self.x + 1) % 10
         self.encounter(self.x, self.y)
 
 
-    def do_left(self, args):
+    def move_left(self):
         self.x = (self.x - 1) % 10
         self.encounter(self.x, self.y)
 
 
+    def do_up(self, args):
+        self.move_up()
+
+
+    def do_down(self, args):
+        self.move_down()
+
+
+    def do_right(self, args):
+        self.move_right()
+
+
+    def do_left(self, args):
+        self.move_left()
+
+
     def do_addmon(self, args):
-        args = shlex.split(args)
-
-        if len(args) != 8:
-            print("Invalid arguments")
-            return
-
-        name = args[0]
-
-        if name not in self.allowed_list and name not in self.user_list:
-            print("Invalid arguments")
-            return
-
-        hello = ''
-        hp = 0
-        m_x, m_y = 0, 0
-
-        i = 1
-        while i < 8:
-            if args[i] == 'hello':
-                hello = args[i+1]
-            elif args[i] == 'hp':
-                try:
-                    hp = int(args[i+1])
-                except Exception:
-                    break
-
-                if hp <= 0:
-                    break
-            elif args[i] == 'coords':
-                try:
-                    m_x = int(args[i+1])
-                    m_y = int(args[i+2])
-                except Exception:
-                    break
-
-                if m_x < 0 or m_x > 9 or m_y < 0 or m_y > 9:
-                    break
-
-                i += 1
-            else:
-                print("Invalid arguments")
-                break
-            i += 2
-
-        if i < 8:
-            return
-
-        if self.field[m_y][m_x] == 0:
-            print(f'Added monster to ({m_x}, {m_y}) saying {hello}')
-        else:
-            print(f'Replaced the old monster')
-
-        self.field[m_y][m_x] = {'hello':hello, 'hp': hp, 'name': name}
+        self.move_mon(*self.get_mon_args(args))
 
 
     def do_EOF(self, args):
