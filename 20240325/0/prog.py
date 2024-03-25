44760312b6b67083802f7abd84f617623655fd2e
Осотов Степан <s02210007@vm-mz4-22.prac.cs.msu.ru>
2024-03-25 14:31:40 +0300

    Add files

--- 20240325/0/prog.py
+++ 20240325/0/prog.py
@@ -4,6 +4,6 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
-test(SimpleHTTPRequestHandler, bind=s.getsockname()[0], port=1234)
+test(SimpleHTTPRequestHandler, bind=s.getsockname()[0], port=8001)
 print(s.getsockname()[0])
 s.close()
