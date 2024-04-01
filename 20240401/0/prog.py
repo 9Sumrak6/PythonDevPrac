ba5eaef0c11575afcb79d2f288f6f07a8a3b7ad7
Осотов Степан <s02210007@vm-mz4-21.prac.cs.msu.ru>
2024-04-01 14:13:39 +0300

    CW

---
+++ 20240401/0/prog.py
@@ -0,0 +1,9 @@
+"""Dummy module."""
+from math import *
+
+
+def ganarray():
+    """Array = [[f"First line should be in imperative mood {i}:{sin(j)}j" for i in range(1)] for j in range(10)]."""
+
+
+genarray()
