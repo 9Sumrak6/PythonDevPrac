51a498c3baf06f3c794f8106fa5dc6b34bc39040
9Sumrak6 <step@Ascorbinka.ascorbinka.org>
2024-04-26 21:22:42 +0300

    Add 1

---
+++ 20240422/1/mood/client/__main__.py
@@ -0,0 +1,11 @@
+"""Main file for client."""
+
+import sys
+import socket
+import threading
+
+from . import Mood, recieve, main
+
+
+if __name__ == "__main__":
+    main()
---
+++ 20240422/1/start_server.py
@@ -0,0 +1,7 @@
+import mood.server
+
+def serve():
+	mood.server.main()
+
+if __name__ == "__main__":
+	serve()
