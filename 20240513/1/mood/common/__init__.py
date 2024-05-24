355ee46df00f372b9239f636b465c46490a4cf46
9Sumrak6 <step@Ascorbinka.ascorbinka.org>
2024-05-24 18:31:33 +0300

    HW

---
+++ 20240513/1/mood/common/__init__.py
@@ -0,0 +1,25 @@
+"""Init file with common constants and functions for client and server files."""
+
+from io import StringIO
+import cowsay
+from pathlib import Path
+
+jgsbat = cowsay.read_dot_cow(StringIO("""
+$the_cow = <<EOC;
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\--//|.'-._  (
+     )'   .'\\/o\\/o\\/'.   `(
+      ) .' . \\====/ . '. (
+       )  / <<    >> \\  (
+        '-._/``  ``\\_.-'
+  jgs     __\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
+
+weapons = {'sword': 10, 'spear': 15, 'axe': 20}
+
+prompt = ':->'
+
+path_doc = str(Path(__file__).parent.resolve() / '../../docs/build/html/index.html')
